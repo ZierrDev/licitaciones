d8f0ace17ff303e6fb26ee08d86f61be10ea8570
Hielo Elemental <hieloelemental@gmail.com>
2024-07-25 21:06:48 -0500

    feat(heading): :sparkles: Create Image Carrousel and NavBar responsive update
    Create Carousel component
    Create Carousel component css
    Create ImagesCarousel component
    Update Navbar, add Open button visible on mobile devices and responsive styles
    Add Header component to contain NavBar and ImagesCarousel components

--- reactproject/src/components/NavBar.jsx
+++ reactproject/src/components/NavBar.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import logoIcon from "/logo.jpg";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 const navbarLinks = [
   { title: "Inicio", to: "/" },
@@ -7,8 +9,9 @@ const navbarLinks = [
 ];
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className='top-0 left-0 w-full fixed flex justify-between items-center text-md font-medium z-50 h-16 bg-teal-400 px-12'>
+    <nav className='top-0 left-0 w-full fixed flex justify-between items-center text-md font-medium z-50 h-16 bg-teal-400 px-2 md:px-12'>
       <ul className='flex items-center gap-4'>
         <li>
           <img src={logoIcon} alt='' className='h-12' />
@@ -21,19 +24,31 @@ const NavBar = () => {
           </a>
         </li>
       </ul>
-      <ul className='flex h-full items-center gap-2'>
+      <ul
+        className={`${
+          isOpen ? "left-0" : "-left-full"
+        } transition-all duration-300 md:left-0 flex h-full items-center md:gap-2 fixed md:relative top-16 md:top-0 flex-col md:flex-row justify-start w-full max-w-sm bg-teal-500 md:bg-transparent md:w-auto`}
+      >
         {navbarLinks.map(({ title, to }) => {
           return (
-            <li key={title} className='h-full flex items-center'>
+            <li
+              key={title}
+              className='w-full md:w-auto h-16 flex items-center justify-center'
+            >
               <a
                 href={to}
-                className='px-4 text-teal-700 h-full hover:h-10 hover:text-teal-950 hover:bg-teal-200 duration-700 hover:border-y-4 border-teal-400 hover:border-teal-50 transition-all flex items-center'
+                className='w-full md:w-auto px-4 text-teal-900 md:text-teal-700 h-full hover:h-10 hover:text-teal-950 hover:bg-teal-200 duration-700 hover:border-y-4 border-teal-400 hover:border-teal-50 transition-all flex items-center justify-center'
               >
                 {title}
               </a>
             </li>
           );
         })}
+      </ul>
+      <ul className='md:hidden'>
+        <li className='p-3' onClick={() => setIsOpen(!isOpen)}>
+          <Bars3Icon className='h-9' />
+        </li>
       </ul>
     </nav>
   );
